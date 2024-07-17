29d22e8d062ac920183d81cfceb5afd85144fe4b
GeorgeEM <elgeo.e.ge.7@gmail.com>
2024-07-17 20:04:52 +0300

    Finish

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,22 +2,23 @@
 """Route module for basic flask app API
 """
 
-from db import DB
-from flask import Flask, jsonify, request, abort, redirect
+from flask import Flask, abort, jsonify, redirect, request
 from flask.helpers import make_response
+
 from auth import Auth
+from db import DB
 from user import User
 
 AUTH = Auth()
 
 app = Flask(__name__)
 
-@app.route('/users', methods=['POST'], strict_slashes=False)
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
 def create_users() -> str:
-    """Register a user and return a JSON response
-    """
-    body_password = request.form.get('password')
-    body_email = request.form.get('email')
+    """Register a user and return a JSON response"""
+    body_password = request.form.get("password")
+    body_email = request.form.get("email")
     try:
         AUTH.register_user(body_email, body_password)
         return jsonify({"email": body_email, "message": "User created"})
@@ -25,38 +26,34 @@ def create_users() -> str:
         return jsonify({"message": "Email already registered"}), 400
 
 
-@app.route('/sessions', methods=['POST'], strict_slashes=False)
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
 def login_user():
-    """Validate user credentials and return a JSON response
-    """
+    """Validate user credentials and return a JSON response"""
     req = request.form
-    body_password = req.get('password', '')
-    body_email = req.get('email', '')
+    body_password = req.get("password", "")
+    body_email = req.get("email", "")
     check_log = AUTH.valid_login(body_email, body_password)
     if not check_log:
         abort(401)
-    res = make_response(jsonify({"email": body_email,
-                                      "message": "Logged in"}))
-    res.set_cookie('session_id', AUTH.create_session(body_email))
+    res = make_response(jsonify({"email": body_email, "message": "Logged in"}))
+    res.set_cookie("session_id", AUTH.create_session(body_email))
     return res
 
 
-@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
-def logout_user():
-    """Delete the session ID
-    """
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout():
+    """Delete the session ID"""
     session_cookie = request.cookies.get("session_id", None)
     user_from_session = AUTH.get_user_from_session_id(session_cookie)
     if session_cookie is None or user_from_session is None:
         abort(403)
     AUTH.destroy_session(user_from_session.id)
-    return redirect('/')
+    return redirect("/")
 
 
-@app.route('/profile', methods=['GET'], strict_slashes=False)
+@app.route("/profile", methods=["GET"], strict_slashes=False)
 def get_profile() -> str:
-    """Return user information if session ID is valid
-    """
+    """Return user information if session ID is valid"""
     u_cookie = request.cookies.get("session_id", None)
     user = AUTH.get_user_from_session_id(u_cookie)
     if u_cookie is None or user is None:
@@ -64,12 +61,11 @@ def get_profile() -> str:
     return jsonify({"email": user}), 200
 
 
-@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
 def get_reset_password_token_route() -> str:
-    """Return a reset password token if the email is registered
-    """
+    """Return a reset password token if the email is registered"""
     user_req = request.form
-    body_emai = user_req.get('email', '')
+    body_emai = user_req.get("email", "")
     is_exists = AUTH.create_session(body_emai)
     if not is_exists:
         abort(403)
@@ -77,13 +73,12 @@ def get_reset_password_token_route() -> str:
     return jsonify({"email": body_emai, "reset_token": token})
 
 
-@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
 def update_password() -> str:
-    """Update the user password if the token is valid
-    """
-    body_emai = request.form.get('email')
-    reset_token = request.form.get('reset_token')
-    new_password = request.form.get('new_password')
+    """Update the user password if the token is valid"""
+    body_emai = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
     try:
         AUTH.update_password(reset_token, new_password)
     except Exception:
@@ -93,4 +88,3 @@ def update_password() -> str:
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
-
